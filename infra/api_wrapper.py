289fd9a819814530fd22f5af1b4d8032eca15fd2
WaelOtman211 <105595561+WaelOtman211@users.noreply.github.com>
2024-03-09 14:44:43 +0200

    update

--- infra/api_wrapper.py
+++ infra/api_wrapper.py
@@ -9,9 +9,9 @@ class APIWrapper:
 
     def __init__(self):
         self.response = None
-        pet_data_file_path = 'C:\Users\saher\OneDrive\קבצים מצורפים\שולחן העבודה\repos\WorldCatSeleniumGrid\pet_data.json'
-        config_file_path = 'C:\Users\saher\OneDrive\קבצים מצורפים\שולחן העבודה\repos\WorldCatSeleniumGrid\config_Pet_swagger.json'
-        store_file_path = 'C:\Users\saher\OneDrive\קבצים מצורפים\שולחן העבודה\repos\WorldCatSeleniumGrid\store.json'
+        pet_data_file_path = r'C:\Users\saher\OneDrive\קבצים מצורפים\שולחן העבודה\repos\WorldCatSeleniumGrid\pet_data.json'
+        config_file_path = r'C:\Users\saher\OneDrive\קבצים מצורפים\שולחן העבודה\repos\WorldCatSeleniumGrid\config_Pet_swagger.json'
+        store_file_path = r'C:\Users\saher\OneDrive\קבצים מצורפים\שולחן העבודה\repos\WorldCatSeleniumGrid\store.json'
         user_file_path = r'C:\Users\saher\OneDrive\קבצים מצורפים\שולחן העבודה\repos\WorldCatSeleniumGrid\user_data.json'
         self.store_data_handler = StoreDataHandler(store_file_path)
         self.user_data_handler = UserDataHandler(user_file_path)
